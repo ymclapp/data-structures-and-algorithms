f24735907b862121a6ce79f1e9bc30692c5e9cfe
ymcla <ymclapp@yahoo.com>
2021-09-03 20:12:56 -0500

    2 and 3 are passing

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -40,7 +40,9 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  const reducer = (accumulator, purchasePrice) => accumulator + purchasePrice;
+  const sum = arr.reduce((accumulator, value) => accumulator + value.purchasePrice, 0);
+  return sum;
 };
 
 /* ------------------------------------------------------------------------------------------------
