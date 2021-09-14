3aab7e48ee9c0504d7fc8dff32050dc2af7d8be2
ymcla <ymclapp@yahoo.com>
2021-09-14 14:13:23 -0500

    2 and 5 are passing

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -64,7 +64,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  const regex = /^\w+(\.(\w+))?@\w+\.(org|com|net)$/;
+  return regex.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
