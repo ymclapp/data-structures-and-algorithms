0f33031eea382de39f79fd5f7c900a4e4c74cd5b
ymclapp <ymclapp@yahoo.com>
2021-10-08 21:20:29 -0500

    formatting changes to files??

--- c-sharp/DataStructures/DataStructures/Node.cs
+++ c-sharp/DataStructures/DataStructures/Node.cs
@@ -7,6 +7,5 @@ namespace DataStructures
   public class Node
   {
     public int Value { get; set; }  //holds the value that we are inserting in the node
-    public Node Next { get; set; }
   }
 }
