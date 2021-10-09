8f239e3a0111621406ebac6cad045bd9f06a1d11
ymclapp <ymclapp@yahoo.com>
2021-10-08 19:39:36 -0500

    added readme and updated overall readme

--- c-sharp/DataStructures/DataStructures/LinkedList.cs
+++ c-sharp/DataStructures/DataStructures/LinkedList.cs
@@ -6,13 +6,13 @@ namespace DataStructures
 {
   public class LinkedList
   {
-    public Node Head { get; set; }
+    public Node Head { get; set; }  //used light bulb in the tests to create this
 
-    public void Insert(int value)
+    public void Insert(int value)  //used the light bulb in the tests to create this
     {
       Node newNode = new Node();  //created linkedList empty
-      newNode.Value = value;  //told it to insert what it is told
-      Head = newNode;  //we are drawing the arrow from the Head to the newNode.  we are pointing it to the newNode. Head is a reference to the first node.
+      newNode.Value = value;  //told it to insert what it is told - needed this for the second test
+      Head = newNode;  //we are drawing the arrow from the Head to the newNode.  we are pointing it to the newNode. Head is a reference to the first node.  Need this for every new Linked List
       //throw new NotImplementedException();  //commented out so that the insert would work with out throwing this exception
     }
   }
---
+++ c-sharp/DataStructures/DataStructures/README.md
@@ -0,0 +1,12 @@
+## Code Challenge:  Class 05 - Linked List Implementation
+# Singly Linked List
+<!-- Short summary or background information -->
+
+## Challenge
+<!-- Description of the challenge -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+<!-- Description of each method publicly available to your Linked List -->
