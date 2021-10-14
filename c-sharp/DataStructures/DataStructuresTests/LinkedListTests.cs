226e77f9a2da2a0e72ef339bf17fef43029e6596
ymclapp <ymclapp@yahoo.com>
2021-10-14 11:59:19 -0500

    lost test work when I pulled.  Fixed and now pushing

--- c-sharp/DataStructures/DataStructuresTests/LinkedListTests.cs
+++ c-sharp/DataStructures/DataStructuresTests/LinkedListTests.cs
@@ -161,27 +161,6 @@ namespace DataStructuresTests
     }
 
     [Fact]
-    public void Finding_kth_value()
-    {
-
-      //Arrange
-      LinkedList list = new LinkedList();
-      list.Insert(210);
-      list.Insert(340);
-      list.Insert(550);
-      list.Insert(10);
-      list.Insert(20);
-      list.Insert(30);
-
-      //Act
-
-
-      //Assert
-      Assert.Equal(10, list.KthValue(3));
-
-    }
-
-    [Fact]
     public void Zipping_two_linkedlists_together()
     {
       //Arrange
@@ -209,6 +188,5 @@ namespace DataStructuresTests
       Assert.Equal("30 -> 550 -> 20 -> 340 -> 10 -> 210 -> NULL", zipped.ToString());
 
     }//end of zipping
-
   }
 }
